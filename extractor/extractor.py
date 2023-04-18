20760dec1ef237c86010b9eb8e18277fb57ac1e2
sett <raik.rohde@gmail.com>
2023-04-18 21:24:17 +0200

    feat(extractor): add functionality to extract commit data
    This commit adds a script to extract commit data from a Git repository. The script clones a repository, iterates through all commits, and saves the commit hash, message, and diff to a SQLite database. It also checks if a commit was made by a bot or is a merge commit and adds those to a separate bot_commits table. The script takes a repository URL as an argument.

---
+++ extractor/extractor.py
@@ -0,0 +1,73 @@
+import sqlite3
+import os
+import sys
+from git import Repo
+from git import Git
+from tqdm import tqdm
+
+# def is_bot_commit(commit):
+#     author_name = commit.author.name.lower()
+#     return "bot" in author_name
+
+def is_bot_commit(commit):
+    author_name = commit.author.name.lower()
+    message = commit.message.lower()
+    return (
+        "bot" in author_name
+        or "snyk" in author_name
+        or "merge" in message
+    )
+
+
+def clone_repository(repo_url, local_path):
+    if not os.path.exists(local_path):
+        Repo.clone_from(repo_url, local_path)
+    repo = Repo(local_path)
+    return repo
+
+def init_database(database_file):
+    conn = sqlite3.connect(database_file)
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS commits (hash TEXT, message TEXT, diff TEXT)''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS bot_commits (hash TEXT, message TEXT)''')
+    conn.commit()
+    return conn
+
+def save_commit_data(conn, commit_hash, message, diff, is_bot=False):
+    cursor = conn.cursor()
+    if is_bot:
+        cursor.execute("INSERT INTO bot_commits (hash, message) VALUES (?, ?)", (commit_hash, message))
+    else:
+        cursor.execute("INSERT INTO commits (hash, message, diff) VALUES (?, ?, ?)", (commit_hash, message, diff))
+    conn.commit()
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <repository_url>")
+        sys.exit(1)
+
+    repo_url = sys.argv[1]
+    repo_name = repo_url.split("/")[-1].split(".")[0]
+    local_path = repo_name
+    database_file = f"{repo_name}.db"
+
+    repo = clone_repository(repo_url, local_path)
+    conn = init_database(database_file)
+    git = Git(local_path)
+
+    commit_count = len(list(repo.iter_commits()))
+    progress_bar = tqdm(total=commit_count, desc="Processing commits", ncols=100)
+
+    for commit in repo.iter_commits():
+        commit_hash = commit.hexsha
+        message = commit.message.strip()
+        is_bot = is_bot_commit(commit)
+        diff = git.diff(commit.parents[0].hexsha, commit.hexsha) if commit.parents else git.diff(commit.hexsha)
+        save_commit_data(conn, commit_hash, message, diff, is_bot)
+        progress_bar.update(1)
+
+    progress_bar.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
