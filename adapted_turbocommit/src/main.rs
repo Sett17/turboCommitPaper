c151824517deb016459a596c509249eba02a140c
sett <raik.rohde@gmail.com>
2023-04-23 00:18:09 +0200

    chore: remove cli and config modules
    Remove cli and config modules from the codebase.

--- adapted_turbocommit/src/main.rs
+++ adapted_turbocommit/src/main.rs
@@ -1,29 +1,16 @@
 use colored::Colorize;
-use config::Config;
 
-use openai::Message;
+use openai::{Message, Model};
 
 use std::{env, process};
 
 mod animation;
-mod cli;
-mod config;
 mod git;
 mod openai;
 mod util;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = Config::load();
-    match config.save() {
-        Ok(_) => (),
-        Err(err) => {
-            println!("{}", format!("Unable to write to config: {err}").red());
-            process::exit(1);
-        }
-    }
-    let options = cli::Options::new(env::args(), &config);
-
     let Ok(api_key) = env::var("OPENAI_API_KEY") else {
         println!("{} {}", "OPENAI_API_KEY not set.".red(), "Refer to step 3 here: https://help.openai.com/en/articles/5112595-best-practices-for-api-key-safety".bright_black());
         process::exit(1);
@@ -31,43 +18,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let repo = git::get_repo()?;
 
-    let system_len = openai::count_token(&config.system_msg).unwrap_or(0);
-    let extra_len = openai::count_token(&options.msg).unwrap_or(0);
+    let system_len = openai::count_token(SYSTEM_MSG).unwrap_or(0);
+    let extra_len = 0;
 
-    let (diff, diff_tokens) =
-        util::decide_diff(&repo, system_len + extra_len, options.model.context_size())?;
-
-    if options.dry_run {
-        println!("This will use ~{} prompt tokens, costing you ~${}.\nEach 1K completion tokens will cost you ~${}",
-            format!("{}", system_len + extra_len + diff_tokens).purple(),
-            format!("{:0.5}", options.model.cost(system_len + extra_len + diff_tokens, 0)).purple(),
-            format!("{:0.5}", options.model.cost(0, 1000)).purple());
-        util::check_version().await;
-        process::exit(0);
-    }
+    let (diff, diff_tokens) = util::decide_diff(
+        &repo,
+        system_len + extra_len,
+        Model::Gpt35Turbo.context_size(),
+    )?;
 
     let prompt_tokens = system_len + extra_len + diff_tokens;
 
-    let mut messages = vec![Message::system(config.system_msg), Message::user(diff)];
+    let mut messages = vec![
+        Message::system(String::from(SYSTEM_MSG)),
+        Message::user(diff),
+    ];
 
-    if !options.msg.is_empty() {
-        messages.push(Message::user(options.msg));
-    }
-
-    let choices = openai::Request::new(
-        options.model.clone().to_string(),
-        messages,
-        options.n,
-        options.t,
-        options.f,
-    )
-    .execute(
-        api_key,
-        options.print_once,
-        options.model.clone(),
-        prompt_tokens,
-    )
-    .await?;
+    let choices = openai::Request::new(Model::Gpt35Turbo.to_string(), messages, 3, 0.8, 0.0)
+        .execute(api_key, false, Model::Gpt35Turbo, prompt_tokens)
+        .await?;
 
     let mut chosen_message = util::choose_message(choices);
 
@@ -77,3 +46,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+const SYSTEM_MSG: &str = r#"AI: gen conv commit-msg. Input: git diff staged files, context, user instr. Task: focus purpose, brief, clear. Output: 1 msg (1 headline, ≤1 body) ONLY.
+Commit-msg guidelines:
+1. Start: type (feat, fix, chore, etc.), opt. scope, opt. ! (breaking), req. colon+space.
+2. feat=new features, fix=bug fixes, etc.
+3. Scope: codebase section, in ().
+4. After type/scope: concise desc, colon+space.
+5. Longer body: blank line after desc.
+
+Multi-changes: 1 msg, concise. 📝Include all crucial changes. ⚠️ ONLY headline & body in output. No extra notes/comments/content.
+
+```example
+feat: add new feature
+
+feature detail
+```"#;
