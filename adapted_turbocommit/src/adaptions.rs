3e98583d121bbd6dde485323697e5280cc716c16
sett <raik.rohde@gmail.com>
2023-04-24 00:25:12 +0200

    feat: improve file selection for large diffs
    Refactor the `diff()` function to improve the file selection process for large diffs. The changes include adding a `DiffFile` struct to represent the source and target file names, implementing `From<&PatchedFile>` and `PartialEq<&PatchedFile>` for `DiffFile`, and using `DiffFile` for the file selection prompt instead of `String`.

--- adapted_turbocommit/src/adaptions.rs
+++ adapted_turbocommit/src/adaptions.rs
@@ -1,13 +1,43 @@
-use std::{fs::File, io::Read};
+use std::{
+    fmt::{self},
+    fs::File,
+    io::Read,
+};
 
 use colored::Colorize;
 use inquire::MultiSelect;
-use unidiff::PatchSet;
+use unidiff::{PatchSet, PatchedFile};
 
 use crate::openai;
 
+#[derive(Debug, PartialEq)]
+struct DiffFile {
+    source_file: String,
+    target_file: String,
+}
+
+impl fmt::Display for DiffFile {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} -> {}", self.source_file, self.target_file)
+    }
+}
+
+impl From<&PatchedFile> for DiffFile {
+    fn from(value: &PatchedFile) -> Self {
+        DiffFile {
+            source_file: value.source_file.clone(),
+            target_file: value.target_file.clone(),
+        }
+    }
+}
+
+impl PartialEq<&PatchedFile> for DiffFile {
+    fn eq(&self, other: &&PatchedFile) -> bool {
+        self.source_file == other.source_file && self.target_file == other.target_file
+    }
+}
+
 pub fn diff(used_tokens: usize, context: usize) -> anyhow::Result<(String, usize)> {
-    //read out file called diff.txt
     let mut file = File::open("diff.txt").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
@@ -28,18 +58,22 @@ pub fn diff(used_tokens: usize, context: usize) -> anyhow::Result<(String, usize
             .bright_black()
         );
 
-        let file_names: Vec<String> = patch.files().iter().map(|file| file.source_file.clone()).collect();
+        let file_names: Vec<DiffFile> = patch
+            .files()
+            .iter()
+            .map(|file| DiffFile::from(file))
+            .collect();
         let selected_file_names = MultiSelect::new(
             "Select the files you want to include in the diff:",
             file_names,
         )
         .prompt()?;
 
-        let selected_files = selected_file_names.iter().map(|file_name| {
+        let selected_files = selected_file_names.iter().map(|file_names| {
             patch
                 .files()
                 .iter()
-                .find(|file| file.source_file == *file_name)
+                .find(|file| *file_names == *file)
                 .unwrap()
         });
 
