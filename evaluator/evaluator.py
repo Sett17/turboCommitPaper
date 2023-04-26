eb57ca62f4885c6058ec597d42f611cce201b9a1
sett <raik.rohde@gmail.com>
2023-04-26 13:02:39 +0200

    refactor(evaluator): streamline evaluation process
    Simplify evaluation process by creating evaluate_commit() function to return adherence and readability scores for a commit message. Add fill_overall parameter to main() to allow for overall scores to be filled in for all evaluated commits.

--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -7,22 +7,45 @@ from tqdm import tqdm
 import textstat
 import spacy
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Evaluate commits in a SQLite database.')
-    parser.add_argument('database', type=str, help='Path to the SQLite database file.')
-    parser.add_argument('--reset', action='store_true', help='Delete the evaluated table and exit.')
-    return parser.parse_args()
-
 def create_evaluated_table(conn: sqlite3.Connection):
     cursor = conn.cursor()
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS evaluated (
         hash TEXT PRIMARY KEY,
-        adherence_score REAL,
-        readability_score REAL,
-        accuracy_score REAL
+        human_adherence REAL,
+        human_readability REAL,
+        human_accuracy REAL,
+        human_overall REAL,
+        ai_adherence REAL,
+        ai_readability REAL,
+        ai_accuracy REAL,
+        ai_overall REAL
     )
     ''')
+    conn.commit()
+
+def insert_evaluated(conn: sqlite3.Connection, hash: str, human_scores: Dict[str, float], ai_scores: Dict[str, float]):
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT INTO evaluated (
+        hash, 
+        human_adherence, human_readability, human_accuracy, human_overall,
+        ai_adherence, ai_readability, ai_accuracy, ai_overall
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (
+        hash,
+        human_scores['adherence'], human_scores['readability'], human_scores['accuracy'], human_scores.get('overall'),
+        ai_scores['adherence'], ai_scores['readability'], ai_scores['accuracy'], ai_scores.get('overall')
+    ))
+    conn.commit()
+
+def update_overall_scores(conn: sqlite3.Connection, hash: str, human_overall: Optional[float], ai_overall: Optional[float]):
+    cursor = conn.cursor()
+    cursor.execute('''
+    UPDATE evaluated
+    SET human_overall = ?, ai_overall = ?
+    WHERE hash = ?
+    ''', (human_overall, ai_overall, hash))
     conn.commit()
 
 def parse_commit_message(message: str) -> Optional[Dict[str, str]]:
@@ -74,7 +97,33 @@ def delete_evaluated_table(conn: sqlite3.Connection):
     conn.commit()
     print('Deleted evaluated table.')
 
-def main(database_path: str, reset: bool):
+def evaluate_commit(commit) -> Dict[str, Optional[float]]:
+    # hash, message, _ = commit
+    hash = commit[0]
+    message = commit[1]
+    
+    parsed_message = parse_commit_message(message)
+
+    if parsed_message:
+        adherence_score = 1.0
+        pos_score = evaluate_pos(parsed_message['message'])
+        if parsed_message['body']:
+            flesch_score = evaluate_flesch(parsed_message['body'])
+        else:
+            flesch_score = evaluate_flesch(parsed_message['message'])
+        readability_score = (pos_score + flesch_score) / 2
+    else:
+        adherence_score = 0.0
+        readability_score = evaluate_flesch(message)
+
+    return {
+        'adherence': adherence_score,
+        'readability': readability_score,
+        'accuracy': None,
+        'overall': None
+    }
+
+def main(database_path: str, reset: bool, fill_overall: bool):
     conn = sqlite3.connect(database_path)
 
     if reset:
@@ -86,37 +135,40 @@ def main(database_path: str, reset: bool):
 
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM commits')
+    human_commits = cursor.fetchall()
 
-    commits = cursor.fetchall()
-    total_commits = len(commits)
+    cursor.execute('SELECT * FROM ai_commits_one_shot')
+    ai_commits = cursor.fetchall()
 
-    for commit in tqdm(commits, total=total_commits, desc="Evaluating commits", unit="commit"):
-        parsed_message = parse_commit_message(commit[1])
+    total_commits = len(human_commits)
 
-        if parsed_message:
-            adherence_score = 1.0
-            pos_score = evaluate_pos(parsed_message['message'])
-
-            if parsed_message['body']:
-                flesch_score = evaluate_flesch(parsed_message['body'])
-            else:
-                flesch_score = evaluate_flesch(commit[1])
-            readability_score = (pos_score + flesch_score) / 2
+    for commit in tqdm(human_commits, total=total_commits, desc="Evaluating human commits", unit="commit"):
+        human_scores = evaluate_commit(commit)
+        ai_commit = [row for row in ai_commits if row[0] == commit[0]]
+        if ai_commit:
+            ai_scores = evaluate_commit(ai_commit[0])
         else:
-            adherence_score = 0.0
-            pos_score = 0.0
-            flesch_score = evaluate_flesch(commit[1])
-            readability_score = flesch_score
+            ai_scores = {'adherence': None, 'readability': None, 'accuracy': None}
 
-        accuracy_score = None
+        insert_evaluated(conn, commit[0], human_scores, ai_scores)
 
-        cursor.execute('INSERT INTO evaluated (hash, adherence_score, readability_score, accuracy_score) VALUES (?, ?, ?, ?)',
-                       (commit[0], adherence_score, readability_score, accuracy_score))
+    if fill_overall:
+        cursor.execute('SELECT * FROM evaluated')
+        evaluated_commits = cursor.fetchall()
+        for commit in tqdm(evaluated_commits, total=len(evaluated_commits), desc="Filling overall scores", unit="commit"):
+            hash, human_adherence, human_readability, human_accuracy, _, ai_adherence, ai_readability, ai_accuracy, _ = commit
+            human_overall = calculate_overall_score(human_adherence, human_readability, human_accuracy)
+            ai_overall = calculate_overall_score(ai_adherence, ai_readability, ai_accuracy)
+            update_overall_scores(conn, hash, human_overall, ai_overall)
 
-    conn.commit()
     conn.close()
 
-
 if __name__ == '__main__':
-    args = parse_arguments()
-    main(args.database, args.reset)
+    parser = argparse.ArgumentParser(description='Evaluate commit messages in an SQLite database.')
+    parser.add_argument('database', type=str, help='Path to the SQLite database file.')
+    parser.add_argument('--reset', action='store_true', help='Delete the evaluated table and exit.')
+    parser.add_argument('-o', '--overall', action='store_true', help='Fill in the overall scores for each row.')
+
+    args = parser.parse_args()
+    
+    main(args.database, args.reset, args.overall)
